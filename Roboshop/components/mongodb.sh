2492220cff68cc1fe1d3dfd4be8bdd6feffa37f0
Durga Siva Kumar E <durgasivakumar97@gmail.com>
2021-09-29 13:06:33 +0530

    chenges in mongodb

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -40,6 +40,8 @@ if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
  else
     echo -e "\e[31mFAILURE\e[0m" 
+    exit 2
+fi 
     Print "starting MongoDB"
  systemctl enable mongod
  systemctl restart mongod
