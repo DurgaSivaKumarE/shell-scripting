ef4eb8e34c441f0cbc4f583d85988cff1bc92fb8
Durga Siva Kumar E <durgasivakumar97@gmail.com>
2021-10-03 15:06:52 +0530

    changes in catalogue

--- Roboshop/components/catalogue.sh
+++ Roboshop/components/catalogue.sh
@@ -23,9 +23,7 @@ Status_Check $?
 
 Print "Extracting Catalogue"
 cd /home/roboshop
-rm -rf catalogue
-unzip -o /tmp/catalogue.zip &>>$LOG
-mv catalogue-main catalogue
+rm -rf catalogue && unzip -o /tmp/catalogue.zip &>>$LOG &&  mv catalogue-main catalogue
 Status_Check $? 
 
 Print "Downloading NodeJS Dependencies"
@@ -35,9 +33,7 @@ Status_Check $?
 
 chown roboshop:roboshop -R /home/roboshop
 
+Print "Set up SystemD service"
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl start catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
+Status_Check $?
 
-
-#mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-#systemctl daemon-reload
-#systemctl start catalogue
-#systemctl enable catalogue
