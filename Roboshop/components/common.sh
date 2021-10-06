af343111ec05ec284bec6dcdcfc6e0d892dfccc0
Durga Siva Kumar E <durgasivakumar97@gmail.com>
2021-10-06 23:30:31 +0530

    update

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -21,3 +21,53 @@ fi
 
 LOG=/tmp/roboshop.log
 rm -f $LOG
+
+ADD_APP_USER(){
+   Print "Adding Roboshop User"
+id roboshop &>>$LOG
+if [ $? -eq 0 ]; then
+    echo "User already there, So skipping" &>>$LOG
+else
+    useradd roboshop &>>$LOG
+fi   
+
+Status_Check $?
+}
+
+DOWNLOAD() {
+   Print "Downloading ${COMPONENT} Content"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+Status_Check $?
+}
+
+NODEJS() {
+   Print "Installing NodeJS"
+   yum install nodejs make gcc-c++ -y &>>$LOG
+   Status_Check $? 
+
+ADD_APP_USER
+DOWNLOAD
+
+
+
+
+Print "Extracting Catalogue"
+cd /home/roboshop
+rm -rf catalogue && unzip -o /tmp/catalogue.zip &>>$LOG &&  mv catalogue-main catalogue
+Status_Check $? 
+
+Print "Downloading NodeJS Dependencies"
+cd /home/roboshop/catalogue
+npm install --unsafe-perm &>>$LOG
+Status_Check $?
+
+chown roboshop:roboshop -R /home/roboshop
+
+Print "Update SystemD service"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
+Status_Check $?
+
+Print "Setuo systemd service"
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl restart catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
+Status_Check $?
+}
