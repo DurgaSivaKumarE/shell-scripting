e25229d5025ad626cb6862064f5ccd4816b0f45f
Durga Siva Kumar E <durgasivakumar97@gmail.com>
2021-10-08 23:01:34 +0530

    update in common.sh script

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -23,7 +23,7 @@ LOG=/tmp/roboshop.log
 rm -f $LOG
 
 ADD_APP_USER(){
-   Print "Adding Roboshop User"
+   Print "Adding Roboshop User\t\t"
 id roboshop &>>$LOG
 if [ $? -eq 0 ]; then
     echo "User already there, So skipping" &>>$LOG
@@ -38,7 +38,7 @@ DOWNLOAD() {
    Print "Downloading ${COMPONENT} Content"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
 Status_Check $?
-Print "Extracting ${COMPONENT}"
+Print "Extracting ${COMPONENT}\t\t"
 cd /home/roboshop
 rm -rf ${COMPONENT} && unzip -o /tmp/${COMPONENT}.zip &>>$LOG &&  mv ${COMPONENT}-main ${COMPONENT}
 Status_Check $?
@@ -47,7 +47,7 @@ Status_Check $?
 SystemD_Setup() {
 
 Print "Update SystemD service"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINTcart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
 Status_Check $?
 
 Print "Setup systemd service"
@@ -88,4 +88,6 @@ JAVA() {
    mv target/shipping-1.0.jar shipping.jar &>>$LOG
    Status_Check $?
    chown roboshop:roboshop -R /home/roboshop
+   SystemD_Setup
+
 } 
