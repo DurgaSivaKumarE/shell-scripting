1a4690525287b0b62975b6926f5e11f924502951
Durga Siva Kumar E <durgasivakumar97@gmail.com>
2021-10-09 13:04:43 +0530

    update

--- Roboshop/components/rabbitmq.sh
+++ Roboshop/components/rabbitmq.sh
@@ -25,7 +25,7 @@ Status_Check $?
 
 Print "Create app user"
 rabbitmqctl list_users | grep roboshop &>>$LOG
-if [ $? -no 0 ]; then
+if [ $? -ne 0 ]; then
     rabbitmqctl add_user roboshop roboshop123 &>>$LOG 
 fi
 rabbitmqctl set_user_tags roboshop administrator  &>>$LOG  && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG 
